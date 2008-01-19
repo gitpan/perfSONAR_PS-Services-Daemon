8d727a9b470a818312c4808c2356dac238305b8e
Aaron Brown <perfsonar@cpan.org>
2008-01-19 22:18:33 +0000

    Import of PERFSONAR/perfSONAR_PS-Services-Daemon-0.04 from CPAN.
    gitpan-cpan-distribution: perfSONAR_PS-Services-Daemon
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         PERFSONAR/perfSONAR_PS-Services-Daemon-0.04.tar.gz
    gitpan-cpan-author:       PERFSONAR
    gitpan-cpan-maturity:     released

--- lib/perfSONAR_PS/Request.pm
+++ lib/perfSONAR_PS/Request.pm
@@ -1,6 +1,6 @@
 package perfSONAR_PS::Request;
 
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 use fields 'REQUEST', 'REQUESTDOM', 'RESPONSE', 'RESPONSEMESSAGE', 'START_TIME', 'CALL', 'NAMESPACES';
 
--- lib/perfSONAR_PS/Services/Echo.pm
+++ lib/perfSONAR_PS/Services/Echo.pm
@@ -1,6 +1,6 @@
 package perfSONAR_PS::Services::Echo;
 
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 use base 'perfSONAR_PS::Services::Base';
 
